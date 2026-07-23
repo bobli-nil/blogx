0e0b70add4618b7ef5f4e4f915cbd26790d395c5
Bob Li <libob2026@gmail.com>
2026-07-23 18:54:01 +0800

    choreï:去掉不用的测试代码

--- main.go
+++ main.go
@@ -4,17 +4,10 @@ import (
 	"blogx_server/core"
 	"blogx_server/flags"
 	"blogx_server/global"
-
-	"github.com/sirupsen/logrus"
 )
 
 func main() {
 	flags.Parse()
 	global.Conf = core.ReadConf()
 	core.InitLogrus()
-
-	logrus.Debugln("这是一个测试")
-	logrus.Infoln("这是一个Info")
-	logrus.Warnln("这是一个警告")
-	logrus.Errorln("这是一个错误")
 }
