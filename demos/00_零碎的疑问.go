ce1614bd0471297f9ecdbf6b7631fbd07e1df6a4
Bob Li <libob2026@gmail.com>
2026-08-16 22:20:17 +0800

    feat:自定义tag_list类型

--- demos/00_零碎的疑问.go
+++ demos/00_零碎的疑问.go
@@ -1,25 +1,10 @@
 package main
 
 import (
-	"blogx_server/utils/jwt"
 	"fmt"
+	"strings"
 )
 
 func main() {
-	//token, err := jwt.GenerateToken(18, "lisi")
-	//if err != nil {
-	//	fmt.Println(err)
-	//	return
-	//}
-	//fmt.Println(token)
-
-	token := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySUQiOjE4LCJ1c2VyTmFtZSI6Imxpc2kiLCJpc3MiOiJsbXIiLCJleHAiOjE3ODUzNzg2ODEsIm5iZiI6MTc4NTI5MjI4MSwiaWF0IjoxNzg1MjkyMjgxfQ.pcTG6TwANU_w8gEqbGJZZ5R4XRa4yka7UdmtVRHPl8Y"
-	claims, err := jwt.ParseToken(token)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Printf("%+v\n", claims)
-	fmt.Println(claims.UserID, claims.UserName)
-	fmt.Printf("%T\n", claims.RegisteredClaims.ExpiresAt)
+	fmt.Println(strings.Join([]string{"a", "b", "c"}, "|"))
 }
---
+++ models/ctype/list.go
@@ -0,0 +1,22 @@
+package ctype
+
+import (
+	"database/sql/driver"
+	"errors"
+	"strings"
+)
+
+type List []string
+
+func (j *List) Scan(value any) error {
+	val, ok := value.([]uint8)
+	if ok {
+		*j = strings.Split(string(val), ",")
+		return nil
+	}
+	return errors.New("类型错误")
+}
+
+func (j *List) Value() (driver.Value, error) {
+	return strings.Join(*j, ","), nil
+}
