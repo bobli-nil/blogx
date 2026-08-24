8d2f274d47dfeefedb1fe9e0bd715d0d2605f0b5
Bob Li <libob2026@gmail.com>
2026-08-24 15:20:31 +0800

    feat:评论消息通知

--- api/comment_api/comment_create.go
+++ api/comment_api/comment_create.go
@@ -7,6 +7,7 @@ import (
 	"blogx_server/models"
 	"blogx_server/models/enum"
 	"blogx_server/service/comment_service"
+	"blogx_server/service/message_service"
 	"blogx_server/service/redis_service/redis_article"
 	"blogx_server/service/redis_service/redis_comment"
 	"blogx_server/utils/jwt"
@@ -59,6 +60,9 @@ func (CommentApi) CommentCreateView(c *gin.Context) {
 		res.FailWithMsg("发布评论失败", c)
 		return
 	}
+
+	go message_service.InsertCommentMessage(model)
+
 	redis_article.SetCacheComment(cr.ArticleID, 1)
 	res.OkWithMsg("发布评论成功", c)
 }
---
+++ service/message_service/enter.go
@@ -0,0 +1,28 @@
+package message_service
+
+import (
+	"blogx_server/global"
+	"blogx_server/models"
+	"blogx_server/models/enum/message_type_enum"
+
+	"github.com/sirupsen/logrus"
+)
+
+// InsertCommentMessage 插入一条评论消息
+func InsertCommentMessage(model models.CommentModel) {
+	global.DB.Preload("UserModel").Preload("ArticleModel").Take(&model)
+	err := global.DB.Create(&models.MessageModel{
+		Type:               message_type_enum.CommentType,
+		RevUserID:          model.ArticleModel.UserID,
+		ActionUserID:       model.UserID,
+		ActionUserNickname: model.UserModel.Nickname,
+		ActionUserAvatar:   model.UserModel.Avatar,
+		Content:            model.Content,
+		ArticleID:          model.ArticleID,
+		ArticleTitle:       model.ArticleModel.Title,
+		CommentID:          model.ID,
+	}).Error
+	if err != nil {
+		logrus.Error(err)
+	}
+}
