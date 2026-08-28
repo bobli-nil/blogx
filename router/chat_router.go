c85cdc9a03d06c69b03d9a1ef25266de06d3ee16
Bob Li <libob2026@gmail.com>
2026-08-28 21:13:11 +0800

    fix:按用户删除聊条记录，未测试，加路由

--- router/chat_router.go
+++ router/chat_router.go
@@ -16,4 +16,5 @@ func ChatRouter(r *gin.RouterGroup) {
 	cr.GET("", middleware.AuthMiddleware, middleware.BindQueryMiddleware[chat_api.ChatListRequest], chatApi.ChatListView)
 	cr.GET("session", middleware.AuthMiddleware, middleware.BindQueryMiddleware[chat_api.SessionListRequest], chatApi.SessionListView)
 	cr.DELETE("", middleware.AuthMiddleware, middleware.BindJSONMiddleware[models.DeleteRequest], chatApi.UserChatDeleteView)
+	cr.DELETE("user/:id", middleware.AuthMiddleware, middleware.BindUriMiddleware[models.IDRequest], chatApi.UserChatDeleteByUserView)
 }
